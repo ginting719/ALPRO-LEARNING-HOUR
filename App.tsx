d7db0b3fdfc3d4d9252ce059aef22605508d435f
ginting719 <gintingeka.apotekalpro@gmail.com>
2025-09-14 01:39:11 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -22,9 +22,7 @@ const ProtectedRoute = ({ children, adminOnly }: { children: JSX.Element, adminO
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-neutral-900 text-white font-sans">
-        <div className="absolute inset-0 -z-10 h-full w-full bg-neutral-900 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
-        <div className="absolute top-0 left-0 -z-10 h-full w-full bg-gradient-to-br from-indigo-500/20 via-purple-500/10 to-pink-500/20 blur-3xl animate-background-pan"></div>
+    <div className="min-h-screen bg-gradient-to-br from-blue-950 via-black to-pink-900 text-white font-sans">
       
       <AuthProvider>
         <HashRouter>
